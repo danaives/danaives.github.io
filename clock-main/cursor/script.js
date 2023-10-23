24d57c98aa3f57e8fce71749b4785052ad999ceb
danaives <146244155+danaives@users.noreply.github.com>
2023-10-23 13:21:00 -0500

    Layout Work

--- clock-main/cursor/script.js
+++ clock-main/cursor/script.js
@@ -2,6 +2,7 @@ const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    +(event.pageX - 10)+"px;")
 })
 
 document.addEventListener('click', () => {
